8a481f62f04e4cf873d8f9baf5f8f8b14cba7d50
Christie Martin <70658855+ChristieMartin@users.noreply.github.com>
2022-02-13 01:32:27 +0300

    Create quicksort.hs

---
+++ 6th_sem/1/quicksort.hs
@@ -0,0 +1,7 @@
+import Data.Char
+
+capitalize :: [String] -> [String]
+capitalize = map(\x -> toUpper (head x) : tail x)
+
+main :: IO ()
+main = print(capitalize ["hello", "darkness", "my", "old", "friend", "i", "have", "come", "to", "talk"])
