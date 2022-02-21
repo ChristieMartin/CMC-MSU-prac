ad509ab949e5af32bfd5d65ebfad46129627e486
Christie Martin <70658855+ChristieMartin@users.noreply.github.com>
2022-02-21 19:56:47 +0300

    Update capitalize.hs

--- 6th_sem/haskell_prac/1/capitalize.hs
+++ 6th_sem/haskell_prac/1/capitalize.hs
@@ -1,7 +1,11 @@
 import Data.Char
 
 capitalize :: [String] -> [String]
-capitalize = map(\x -> toUpper (head x) : tail x)
+capitalize = map(\x -> capitalizeWord x)
+
+capitalizeWord :: String -> String
+capitalizeWord "" = ""
+capitalizeWord x = toUpper (head x) : tail x
 
 main :: IO ()
 main = print(capitalize ["hello", "darkness", "my", "old", "friend", "i", "have", "come", "to", "talk"])
